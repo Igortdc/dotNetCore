34c3f56f1fa619a2a72bc308b417a1ae07d42424
Igortdc <igortdc@gmail.com>
2017-06-19 10:55:18 -0300

    Projeto Inicial

---
+++ ApiDotNetCore/Controllers/Print.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Service
+{
+    public class Print : IPrint
+    {
+        public string[] printConsole()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
